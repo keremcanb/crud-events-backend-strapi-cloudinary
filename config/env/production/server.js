25a4e62a38290c35707d27d1926bd69e58c1ba10
keremcanb <keremcan@gmail.com>
2021-05-13 23:56:47 +0300

    cors

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,10 @@
 module.exports = ({ env }) => ({
-  url: env('HEROKU_URL')
+  url: env("HEROKU_URL"),
+  settings: {
+    cors: {
+      enabled: true,
+      // headers: '*',
+      origin: ["http://localhost", "https://next-events.herokuapp.com"],
+    },
+  },
 });
